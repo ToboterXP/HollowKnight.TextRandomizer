29b2b3c58bdb79cf587a6defeb3633fe6652b01a
ToboterXP <55210408+ToboterXP@users.noreply.github.com>
2023-05-01 13:34:34 +0200

    Added mod menu

---
+++ RandomizerIntegration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomizerMod.RC;
+
+namespace TextRandomizer
+{
+    internal class RandomizerIntegration
+    {
+        public static void Init()
+        {
+            RequestBuilder.OnUpdate.Subscribe(10000f, Randomize);
+            RandomizerMenu.Hook();
+        }
+
+        public static void Randomize(RequestBuilder rb)
+        {
+            TextRandomizer.SaveData.seed = rb.gs.Seed + 1;
+            TextRandomizer.Instance.Randomize();
+        }
+    }
+}
--- TextRandomizer.cs
+++ TextRandomizer.cs
@@ -1,5 +1,5 @@
 ﻿using Modding;
-using RandomizerMod.RC;
+using Satchel.BetterMenus;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,9 +13,10 @@ namespace TextRandomizer
     {
         public bool active = false;
         public Dictionary<string, string> languageKeyMap = new();
+        public int seed = new System.Random().Next();
     }
 
-    public class TextRandomizer : Mod, ILocalSettings<SaveData>
+    public class TextRandomizer : Mod, ILocalSettings<SaveData>, ICustomMenuMod
     {
         internal static TextRandomizer Instance;
 
@@ -27,9 +28,19 @@ namespace TextRandomizer
         //    };
         //}
 
-        public static SaveData saveData { get; set; } = new SaveData();
-        public void OnLoadLocal(SaveData s) => saveData = s;
-        public SaveData OnSaveLocal() => saveData;
+        public static SaveData SaveData { get; set; } = new();
+
+        public bool ToggleButtonInsideMenu { get; }
+
+        public void OnLoadLocal(SaveData s)
+        {
+            SaveData = s;
+        }
+        public SaveData OnSaveLocal() => SaveData;
+
+        private Menu MenuRef;
+        private Element[] menuElements;
+        private TextPanel mainMenuWarning;
 
         public static string ModName = "Text Randomizer";
 
@@ -43,55 +54,68 @@ namespace TextRandomizer
             Log("Initializing");
 
             Instance = this;
-            RequestBuilder.OnUpdate.Subscribe(10000f, Randomize);
 
             ModHooks.LanguageGetHook += OnLanguageGet;
 
-            Menu.Hook();
+            On.QuitToMenu.Start += ResetSave;
+            On.HeroController.Start += (s, e) => { SetVisible(true); s(e); };
+
+            if (ModHooks.GetMod("Randomizer 4") is Mod) RandomizerIntegration.Init();
 
             Log("Initialized");
 
         }
 
-        /*public override int LoadPriority()
+        /*public override int LoadPriority() 
         {
-            return int.MaxValue - 100; //Load after every other mod, to allow randomization of modded strings
+            return int.MaxValue; //Load after every other mod, to allow randomization of modded strings
         }*/
 
+        public IEnumerator ResetSave(On.QuitToMenu.orig_Start orig, global::QuitToMenu self)
+        {
+            SaveData = null;
+            SetVisible(false);
+            return orig(self);
+        } 
 
 
         public override string GetVersion()
         {
-            return "v1.0.0.0";
+            return "v1.1.0.0";
         }
 
         public string OnLanguageGet(string key, string sheetTitle, string orig)
         {
-            if (saveData == null || !saveData.active) return orig;
+            if (SaveData == null || !SaveData.active) return orig;
 
 
-            saveData.active = false;
+            SaveData.active = false;
 
             string mapping;
-            if (!saveData.languageKeyMap.TryGetValue(sheetTitle + ";" + key, out mapping)) mapping = sheetTitle + ";" + key;
+            if (!SaveData.languageKeyMap.TryGetValue(sheetTitle + ";" + key, out mapping)) mapping = sheetTitle + ";" + key;
             string newSheet = mapping.Split(';')[0];
             string newKey = mapping.Split(';')[1];
 
             string ret = Language.Language.Get(newKey, newSheet);
 
-            saveData.active = true;
+            SaveData.active = true;
 
             return ret;
         }
 
-
-
-        public void Randomize(RequestBuilder rb)
+        public void Randomize()
         {
-            if (!saveData.active) return;
-            System.Random rng = rb.rng;
+            if (SaveData == null) return;
+            Randomize(new System.Random(SaveData.seed));
+        }
 
-            saveData.languageKeyMap.Clear();
+
+        public void Randomize(System.Random rng)
+        {
+            if (SaveData == null) return;
+            if (!SaveData.active) return;
+
+            SaveData.languageKeyMap.Clear();
 
             var language = (Dictionary<string, Dictionary<string, string>>)typeof(Language.Language).GetField("currentEntrySheets", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
 
@@ -121,8 +145,6 @@ namespace TextRandomizer
 
             for (int i=0; i<languageKeys.Count; i++)
             {
-
-                Log(languageKeys[i]);
                 int dir = 1;
                 if (rng.Next(2) == 0) dir = -1;
 
@@ -138,8 +160,78 @@ namespace TextRandomizer
                 if (maxShift > 100) maxShift = 100;
                 int offset = rng.Next(maxShift);
 
-                saveData.languageKeyMap[languageKeys[i].Item1] = languageKeys[i + offset*dir].Item1;
+                if (languageKeys[i].Item2 > 0 && languageKeys[i + offset * dir].Item2 == 0) dir = 1; //avoid removing important buttons
+
+                SaveData.languageKeyMap[languageKeys[i].Item1] = languageKeys[i + offset*dir].Item1;
             }
+        }
+
+        public void ReplaceAll(string sheetKey)
+        {
+            var language = (Dictionary<string, Dictionary<string, string>>)typeof(Language.Language).GetField("currentEntrySheets", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
+            foreach (var sheet in language.Keys)
+            {
+                foreach (var key in language[sheet].Keys)
+                {
+                    string s = Language.Language.Get(key, sheet);
+                    int t;
+                    if (int.TryParse(s, out t)) continue;//ignore numeric values
+                    SaveData.languageKeyMap[sheet + ";" + key] = sheetKey;
+                }
+            }
+        }
+
+        public void ZoteMode()
+        {
+            var language = (Dictionary<string, Dictionary<string, string>>)typeof(Language.Language).GetField("currentEntrySheets", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
+
+            System.Random rng = new(SaveData.seed);
+            foreach (var sheet in language.Keys)
+            {
+                foreach (var key in language[sheet].Keys)
+                {
+                    string s = Language.Language.Get(key, sheet);
+                    int t;
+                    if (int.TryParse(s, out t)) continue;//ignore numeric values
+
+                    string sheetKey = "Titles;ZOTE_MAIN";
+                    if (s.Length > 20 && rng.Next(100) < 70) sheetKey = "Zote;PRECEPT_" + rng.Next(1, 58);
+                    SaveData.languageKeyMap[sheet + ";" + key] = sheetKey;
+                }
+            }
+        }
+
+        public void SetActive(bool val)
+        {
+            SaveData.active = val;
+            if (ModHooks.GetMod("Randomizer 4") is Mod) RandomizerMenu.Instance.UpdateSettings();
+        }
+
+        public void SetVisible(bool val)
+        {
+            foreach (var e in menuElements) { e.isVisible = val; }
+            mainMenuWarning.isVisible = !val;
+        }
+
+        public MenuScreen GetMenuScreen(MenuScreen modListMenu, ModToggleDelegates? toggleDelegates)
+        {
+            mainMenuWarning = new TextPanel("Please open a save file first");
+            menuElements = new Element[]
+            {
+                mainMenuWarning,
+                new MenuButton("Randomize", "Shuffles all text in the game", (buttton) => {SetActive(true); Randomize(); } ),
+                new MenuButton("Change Seed", "Changes the current randomization to a new one", (buttton) => {SetActive(true); SaveData.seed++; Randomize(); } ),
+                new MenuButton("No Text", "Remove all text", (buttton) => {SetActive(true); ReplaceAll("UI;UI_BLANK"); } ),
+                new MenuButton("Zote Mode", "Zote Mode", (buttton) => {SetActive(true); ZoteMode(); } ),
+                new MenuButton("Disable", "Turns off the mod", (buttton) => {SetActive(false); } ),
+            };
+            MenuRef ??= new Menu(ModName, menuElements);
+
+            SetVisible(false);
+
+            var ret = MenuRef.GetMenuScreen(modListMenu);
+
+            return ret; 
         }
     }
 }
